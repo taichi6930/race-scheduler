1177bd2e77afa34564108278432c7adb85d1c58a
taichi6930 <49602502+taichi6930@users.noreply.github.com>
2026-08-21 12:26:00 +0000

    テストのOverride型import漏れを修正
    flutter_riverpod 3.xではOverride型がpackage:flutter_riverpod/misc.dartに
    あり、flutter_riverpod.dartからは再エクスポートされていないため、
    flutter analyzeがnon_type_as_type_argumentで失敗していた。

--- packages/front/test/unittest/features/favorites/application/favorite_toggle_feedback_test.dart
+++ packages/front/test/unittest/features/favorites/application/favorite_toggle_feedback_test.dart
@@ -9,6 +9,7 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_riverpod/misc.dart' show Override;
 import 'package:flutter_test/flutter_test.dart';
 import 'package:front/core/di/shared_preferences_provider.dart';
 import 'package:front/domain/repositories/i_favorites_repository.dart';
